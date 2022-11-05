90abc5f6ca3b6d60aabb0e7dc1a106bb77883135
brentonpeterson <30285475+brentonpeterson@users.noreply.github.com>
2022-11-05 03:42:08 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -144,8 +144,7 @@ function buildUserSelectionForm (users) {
 
     if ('link' in user && user.link.trim() !== '') {
       const profileLink = document.createElement('a')
-	  profileLink.textContent = user.link.replace('https://', '')
-	  profileLink.setAttribute('data-title', 'Profile')
+	  profileLink.textContent = user.link.replace(/.*?/, 'Profile')
       profileLink.setAttribute('href', user.link)
       profileLink.setAttribute('target', '_blank')
       wrapper.appendChild(profileLink)
